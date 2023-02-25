4d1b70ad4be5cbc08fbef6d54f6b603178ada8ec
Ali Zulfikar <62384197+alizul01@users.noreply.github.com>
2023-02-25 13:45:48 +0700

    refactor:<**.js into **.ts>

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 
 app.get("/", (req, res) => {
@@ -9,4 +9,4 @@ app.get("/", (req, res) => {
     })
 })
 
-module.exports = app
+export default app
